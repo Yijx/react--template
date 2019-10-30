b433fca2dd9ccdb5c83fe40cba76f3947be37e48
yangjiaxun <yangjiaxun@bbdservice.com>
2019-10-30 10:04:34 +0800

    chore: update deps

--- script/gitPush.sh
+++ script/gitPush.sh
@@ -5,8 +5,6 @@ echo -e "\033[42;37m git add completed! \033[0m"
 echo -e "\033[42;37m start git commit... \033[0m"
 npm run commit
 echo -e "\033[42;37m git commit completed! \033[0m"
-echo -e "\033[42;37m git push to default branch(yangjiaxun)... \033[0m"
+echo -e "\033[42;37m git push to default branch(master)... \033[0m"
 git push
-echo -e "\033[42;37m git push to dev branch(yangjiaxun→dev)... \033[0m"
-git push origin yangjiaxun:dev
 echo -e "\033[42;37m completed! \033[0m"
